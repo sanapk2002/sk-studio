3a2298bae8dd25601bc70209313078577a4cade7
Sana Khatri <sanapkhatri@gmail.com>
2025-11-12 15:42:30 -0300

    removed scroll

--- js/main.js
+++ js/main.js
@@ -151,51 +151,51 @@
 	});
 
 	// Navbar scroll behavior
-var scrollWindow = function () {
-  // Run only on screens wider than 768px (desktop/tablet)
-  if ($(window).width() > 768) {
-    $(window).on('scroll', function () {
-      var $w = $(this),
-        st = $w.scrollTop(),
-        navbar = $('.ftco_navbar'),
-        sd = $('.js-scroll-wrap');
-
-      if (st > 150) {
-        if (!navbar.hasClass('scrolled')) {
-          navbar.addClass('scrolled');
-        }
-      }
-
-      if (st < 150) {
-        if (navbar.hasClass('scrolled')) {
-          navbar.removeClass('scrolled sleep');
-        }
-      }
-
-      if (st > 350) {
-        if (!navbar.hasClass('awake')) {
-          navbar.addClass('awake');
-        }
-        if (sd.length > 0) {
-          sd.addClass('sleep');
-        }
-      }
-
-      if (st < 350) {
-        if (navbar.hasClass('awake')) {
-          navbar.removeClass('awake');
-          navbar.addClass('sleep');
-        }
-        if (sd.length > 0) {
-          sd.removeClass('sleep');
-        }
-      }
-    });
-  }
-};
-
-// Delay the initialization slightly to avoid early layout jumps
-setTimeout(scrollWindow, 600);
+        /*var scrollWindow = function () {
+          // Run only on screens wider than 768px (desktop/tablet)
+          if ($(window).width() > 768) {
+            $(window).on('scroll', function () {
+              var $w = $(this),
+                st = $w.scrollTop(),
+                navbar = $('.ftco_navbar'),
+                sd = $('.js-scroll-wrap');
+        
+              if (st > 150) {
+                if (!navbar.hasClass('scrolled')) {
+                  navbar.addClass('scrolled');
+                }
+              }
+        
+              if (st < 150) {
+                if (navbar.hasClass('scrolled')) {
+                  navbar.removeClass('scrolled sleep');
+                }
+              }
+        
+              if (st > 350) {
+                if (!navbar.hasClass('awake')) {
+                  navbar.addClass('awake');
+                }
+                if (sd.length > 0) {
+                  sd.addClass('sleep');
+                }
+              }
+        
+              if (st < 350) {
+                if (navbar.hasClass('awake')) {
+                  navbar.removeClass('awake');
+                  navbar.addClass('sleep');
+                }
+                if (sd.length > 0) {
+                  sd.removeClass('sleep');
+                }
+              }
+            });
+          }
+        };
+        
+        // Delay the initialization slightly to avoid early layout jumps
+        setTimeout(scrollWindow, 600);*/
 
 
 	
